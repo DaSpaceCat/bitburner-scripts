060b8e0eac4ea9ed1932703558ad3bae9137d9d6
Rhiannon <renii@reniivali.com>
2022-10-16 16:14:50 -0600

    even more hud changes, really, what did you expect

--- src/src/hud.js
+++ src/src/hud.js
@@ -50,6 +50,13 @@
 	"entropy":0
 }*/
 /* eslint-disable no-constant-condition */
+const col = {
+	money: "#e5C07B",
+	def: "#FFFFFF"
+	hak: "#98c379"
+	sta: "#abb2bf"
+	cha: "#c678dd"
+}
 /** @param {NS} ns */
 /** @param {import(".").NS} ns */
 export async function main(ns) {
@@ -68,81 +75,81 @@ export async function main(ns) {
 		try {
 			const headers = [];
 			const values = [];
-			pushCont(headers, values, "In: " + ns.getPlayer()['city'], "At: " + ns.getPlayer()['location'], "#ffffff");
-			pushBreak(headers, values, '──────────────── CRIMES ────────────────');
-			pushCont(headers, values, "Total Karma: ", '   ' + ns.nFormat(ns.heart.break(), '0,0'), "#ffffff");
-			pushCont(headers, values, "People Killed: ", '   ' + ns.nFormat(ns.getPlayer()['numPeopleKilled'], '0,0'), "#ffffff");
-			pushBreak(headers, values, '──────────── MONEY & PROFIT ────────────');
-			pushCont(headers, values, "Money: ", '   ' + ns.nFormat(ns.getPlayer()['money'], '$0,0'), "#E5C07B");
+			pushContE(headers ,values, "╭─ CUSTOM STATS ", "────────────────────────────────────────╮", col.def)
+			pushCont(headers, values, "In: " + ns.getPlayer()['city'], "At: " + ns.getPlayer()['location'], col.def);
+			pushBreak(headers, values, 'CRIMES', '────────────────');
+			pushCont(headers, values, "Total Karma: ", '   ' + ns.nFormat(ns.heart.break(), '0,0'), col.cha);
+			pushCont(headers, values, "People Killed: ", '   ' + ns.nFormat(ns.getPlayer()['numPeopleKilled'], '0,0'), col.cha);
+			pushBreak(headers, values, 'MONEY & PROFIT', '────────────');
+			pushCont(headers, values, "Money: ", '   ' + ns.nFormat(ns.getPlayer()['money'], '$0,0'), col.money);
 			if (ns.gang.inGang()) {
 				if (ns.gang.getGangInformation()['moneyGainRate'] > 0) {
-					pushCont(headers, values, "Gang Income: ", '   ' + ns.nFormat((5 * ns.gang.getGangInformation()['moneyGainRate']), '$0,0') + ' /s', "#E5C07B");
+					pushCont(headers, values, "Gang Income: ", '   ' + ns.nFormat((5 * ns.gang.getGangInformation()['moneyGainRate']), '$0,0') + ' /s', col.money);
 				}
 			}
-			pushCont(headers, values, 'Hack Income: ', '   ' + ns.nFormat(ns.getTotalScriptIncome()[0], '$0,0') + ' /s', "#E5C07B");
+			pushCont(headers, values, 'Hack Income: ', '   ' + ns.nFormat(ns.getTotalScriptIncome()[0], '$0,0') + ' /s', col.money);
 			/*if (ns.hacknet.numHashes() > 0) {
 				headers.push('Hashes: ');
 				values.push(' ' + ns.hacknet.numHashes().toPrecision(3) + ' / ' + ns.hacknet.hashCapacity().toPrecision(3));
 			}*/
-			pushBreak(headers, values, '─────────── SKILL EXPERIENCE ───────────');
-			pushCont(headers, values, "Hacking: ", '   ' + ns.nFormat(ns.getPlayer()['exp']['hacking'], '0,0'), "#98C379");
-			pushCont(headers, values, "Str | Def: ", '   ' + ns.nFormat(ns.getPlayer()['exp']['strength'], '0,0') + ' | ' + ns.nFormat(ns.getPlayer()['exp']['defense'], '0,0'), "#ABB2BF");
-			pushCont(headers, values, "Dex | Agi: ", '   ' + ns.nFormat(ns.getPlayer()['exp']['dexterity'], '0,0') + ' | ' + ns.nFormat(ns.getPlayer()['exp']['agility'], '0,0'), "#ABB2BF");
-			pushCont(headers, values, "Charisma: ", '   ' + ns.nFormat(ns.getPlayer()['exp']['charisma'], '0,0'), "#C678DD");
+			pushBreak(headers, values, 'SKILL EXPERIENCE', '───────────');
+			pushCont(headers, values, "Hacking: ", '   ' + ns.nFormat(ns.getPlayer()['exp']['hacking'], '0,0'), col.hak);
+			pushCont(headers, values, "Str | Def: ", '   ' + ns.nFormat(ns.getPlayer()['exp']['strength'], '0,0') + ' | ' + ns.nFormat(ns.getPlayer()['exp']['defense'], '0,0'), col.sta);
+			pushCont(headers, values, "Dex | Agi: ", '   ' + ns.nFormat(ns.getPlayer()['exp']['dexterity'], '0,0') + ' | ' + ns.nFormat(ns.getPlayer()['exp']['agility'], '0,0'), col.sta);
+			pushCont(headers, values, "Charisma: ", '   ' + ns.nFormat(ns.getPlayer()['exp']['charisma'], '0,0'), col.cha);
 			/*headers.push('Intelligence: ')
 			values.push('   ' + ns.nFormat(ns.getPlayer()['exp']['intelligence'], '0,0'));*/
 			if (ns.gang.inGang()) {
-				pushBreak(headers, values, '───────────────── GANG ─────────────────');
+				pushBreak(headers, values, 'GANG', '─────────────────');
 				if (ns.gang.getBonusTime() > 3000) {
-					pushCont(headers, values, "Bonus Time: ", '   ' + ns.tFormat(ns.gang.getBonusTime()), "#ffffff");
+					pushCont(headers, values, "Bonus Time: ", '   ' + ns.tFormat(ns.gang.getBonusTime()), col.hak);
 				}
 				let gangType = (ns.gang.getGangInformation()['isHacking']) ? "Hacking" : "Combat";
-				pushCont(headers, values, "Faction: ", '   ' + ns.gang.getGangInformation()['faction'] + ', ' + gangType, "#ffffff");	
-				pushCont(headers, values, "Respect: ", '   ' + ns.nFormat(ns.gang.getGangInformation()['respect'], '0,0'), "#ffffff");
+				pushCont(headers, values, "Faction: ", '   ' + ns.gang.getGangInformation()['faction'] + ', ' + gangType, col.def);	
+				pushCont(headers, values, "Respect: ", '   ' + ns.nFormat(ns.gang.getGangInformation()['respect'], '0,0'), col.sta);
 				if (ns.gang.getGangInformation()['power'] > 1) {
-					pushCont(headers, values, "Power: ", '   ' + ns.nFormat(ns.gang.getGangInformation()['power'], '0,0.00'), "#ffffff");
+					pushCont(headers, values, "Power: ", '   ' + ns.nFormat(ns.gang.getGangInformation()['power'], '0,0.00'), col.sta);
 				}
-				pushCont(headers, values, "Territory: ", '   ' + ns.nFormat(ns.gang.getGangInformation()['territory'], '0.000%'), "#ffffff");
+				pushCont(headers, values, "Territory: ", '   ' + ns.nFormat(ns.gang.getGangInformation()['territory'], '0.000%'), col.cha);
 				if (ns.gang.getGangInformation()['wantedLevel'] > 1) {
-					pushCont(headers, values, "Wanted Level: ", '   ' + ns.nFormat(ns.gang.getGangInformation()['wantedLevel'], '0,0'), "#ffffff");
+					pushCont(headers, values, "Wanted Level: ", '   ' + ns.nFormat(ns.gang.getGangInformation()['wantedLevel'], '0,0'), col.cha);
 				}
 				if (ns.gang.getGangInformation()['territoryClashChance'] > 0) {
-					pushCont(headers, values, "Clash Chance: ", '   ' + ns.nFormat(ns.gang.getGangInformation()['territoryClashChance'], '0.0%') + ' / ' + ((ns.gang.getGangInformation()['territoryWarfareEngaged']) ? "" : ""), "#ffffff");
+					pushCont(headers, values, "Clash Chance: ", '   ' + ns.nFormat(ns.gang.getGangInformation()['territoryClashChance'], '0.0%') + ' / ' + ((ns.gang.getGangInformation()['territoryWarfareEngaged']) ? "" : ""), col.cha);
 				}
 			}
 			if (ns.getPlayer()['hasCorporation']) {
 				let corp = eval("ns.corporation.getCorporation()");
-				pushBreak(headers, values, '───────────────── CORP ─────────────────');
-				pushCont(headers, values, "Name: ", '   ' + corp['name'], "#ffffff");
-				pushCont(headers, values, "Funds: ", '   ' + ns.nFormat(corp['funds'], '$0,0'), "#E5C07B");
-				pushCont(headers, values, "Revenue: ", '   ' + ns.nFormat(corp['revenue'], '$0,0') + '/s', "#E5C07B");
-				pushCont(headers, values, "Expenses: ", '   ' + ns.nFormat(corp['expenses'], '$0,0') + '/s', "#E5C07B");
-				pushCont(headers, values, "Profit: ", '   ' + ns.nFormat(corp['revenue'] - corp['expenses'], '$0,0') + '/s', "#E5C07B");
-				pushCont(headers, values, "Shares: ", '   ' + ns.nFormat(corp['numShares'], '0,0') + ' / ' + ns.nFormat(corp['totalShares'], '0,0'), "#ffffff");
+				pushBreak(headers, values, 'CORP', '─────────────────');
+				pushCont(headers, values, "Name: ", '   ' + corp['name'], col.def);
+				pushCont(headers, values, "Funds: ", '   ' + ns.nFormat(corp['funds'], '$0,0'), col.money);
+				pushCont(headers, values, "Revenue: ", '   ' + ns.nFormat(corp['revenue'], '$0,0') + '/s', col.money);
+				pushCont(headers, values, "Expenses: ", '   ' + ns.nFormat(corp['expenses'], '$0,0') + '/s', col.money);
+				pushCont(headers, values, "Profit: ", '   ' + ns.nFormat(corp['revenue'] - corp['expenses'], '$0,0') + '/s', col.money);
+				pushCont(headers, values, "Shares: ", '   ' + ns.nFormat(corp['numShares'], '0,0') + ' / ' + ns.nFormat(corp['totalShares'], '0,0'), col.hak);
 			}
 			if (ns.getPlayer()['inBladeburner']) {
-				pushBreak(headers, values, '───────────── BLADEBURNERS ─────────────');
-				pushCont(headers, values, "Rank: ", '   ' + ns.nFormat(ns.bladeburner.getRank(), '0,0'), "#ffffff");
+				pushBreak(headers, values, 'BLADEBURNERS', '─────────────');
+				pushCont(headers, values, "Rank: ", '   ' + ns.nFormat(ns.bladeburner.getRank(), '0,0'), col.cha);
 				let stm = ns.bladeburner.getStamina();
-				pushCont(headers, values, "Stamina: ", `   ${ns.nFormat(stm[0], '0,0.00')}/${ns.nFormat(stm[1], '0,0.00')} | ${ns.nFormat(stm[0] / stm[1], '0.000%')}`, "#ffffff");
-				/*headers.push("Action: ");
+				pushCont(headers, values, "Stamina: ", `   ${ns.nFormat(stm[0], '0,0.00')}/${ns.nFormat(stm[1], '0,0.00')} | ${ns.nFormat(stm[0] / stm[1], '0.000%')}`, col.sta);
 				if (ns.bladeburner.getCurrentAction()['type'] == "Idle") {
-					values.push('   ' + ns.bladeburner.getCurrentAction()['type']);
+					pushCont(headers, values, "Action: ", '   ' + ns.bladeburner.getCurrentAction()['type'], col.sta);
 				} else {
-					values.push(`   ${ns.bladeburner.getCurrentAction()['type']}: ${ns.bladeburner.getCurrentAction()['name']}`);
-				}*/
-				pushCont(headers, values, "City: ", '   ' + ns.bladeburner.getCity(), "#ffffff");
-				pushCont(headers, values, "Skill Points: ", '   ' + ns.nFormat(ns.bladeburner.getSkillPoints(), '0,0'), "#ffffff");
-				
+					pushCont(headers, values, "Action: ", `   ${ns.bladeburner.getCurrentAction()['type']}: ${ns.bladeburner.getCurrentAction()['name']}`, col.hak);
+				}
+				pushCont(headers, values, "Skill Points: ", '   ' + ns.nFormat(ns.bladeburner.getSkillPoints(), '0,0'), col.hak);
+				pushCont(headers, values, "City: ", '   ' + ns.bladeburner.getCity(), col.def);				
 			}
-			pushBreak(headers, values, '──────────────── SERVER ────────────────');
-			pushCont(headers, values, 'Home: ', "   Cores: " + ns.getServer('home')['cpuCores'] + " | Ram: " + ns.nFormat(ns.getServerUsedRam('home'), '0,0') + ' / ' + ns.nFormat(ns.getServerMaxRam('home'), '0,0'), "#ffffff");
+			pushBreak(headers, values, 'SERVER', '────────────────');
+			pushCont(headers, values, 'Home: ', "   Cores: " + ns.getServer('home')['cpuCores'] + " | Ram: " + ns.nFormat(ns.getServerUsedRam('home'), '0,0') + ' / ' + ns.nFormat(ns.getServerMaxRam('home'), '0,0'), col.hak);
 			for (let i = 0; i <= srvs.length - 1; i++) {
-				pushCont(headers, values, srvs[i] + ": ", "   Cores: " + ns.getServer(srvs[i])['cpuCores'] + " | Ram: " + ns.nFormat(ns.getServerUsedRam(srvs[i]), '0,0') + ' / ' + ns.nFormat(ns.getServerMaxRam(srvs[i]), '0,0'), "#ffffff");
+				pushCont(headers, values, srvs[i] + ": ", "   Cores: " + ns.getServer(srvs[i])['cpuCores'] + " | Ram: " + ns.nFormat(ns.getServerUsedRam(srvs[i]), '0,0') + ' / ' + ns.nFormat(ns.getServerMaxRam(srvs[i]), '0,0'), col.hak);
 			}
-			pushBreak(headers, values, '─────────────── PLAYTIME ───────────────');
-			pushCont(headers, values, `BN${ns.getPlayer()['bitNodeN']}: `, ns.tFormat(ns.getPlayer()['playtimeSinceLastBitnode']), "#ffffff");
-			pushCont(headers, values, 'Total Playtime: ', ns.tFormat(ns.getPlayer()['totalPlaytime']), "#ffffff");
+			pushBreak(headers, values, 'PLAYTIME', '───────────────');
+			pushCont(headers, values, `BN${ns.getPlayer()['bitNodeN']}: `, ns.tFormat(ns.getPlayer()['playtimeSinceLastBitnode']), col.def);
+			pushCont(headers, values, 'Total: ', ns.tFormat(ns.getPlayer()['totalPlaytime']), col.def);
+			pushContE(headers ,values, "╰─ CUSTOM STATS ", "────────────────────────────────────────╯", col.def)
 			hook0.innerHTML = headers.join(" \n");
 			hook1.innerHTML = values.join("\n");
 		}
@@ -153,12 +160,17 @@ export async function main(ns) {
 	}
 }
 
-function pushBreak(hed, val, sec) {
-	hed.push('<span style="color: #ffffff">───────────────</span><br>')
-	val.push('<span style="color: #ffffff">' + sec + '</span><br>')
+function pushBreak(hed, val, sec, dv) {
+	hed.push(`<span style="color: ${col.def}">├───────────────</span><br>`)
+	val.push(`<span style="color: ${col.def}">${dv} <span style="color: ${col.hak}">${sec}</span> ${dv}┤</span><br>`)
 }
 
 function pushCont(hed, val, tp, cont, col) {
+	hed.push(`<span style="color: #ffffff">│</span><span style="color: ${col}">${tp}</span><br>`)
+	val.push(`<span style="color: ${col}">${cont}</span><span style="color: #ffffff">│</span><br>`)
+}
+
+function pushContE(hed, val, tp, cont, col) {
 	hed.push(`<span style="color: ${col}">${tp}</span><br>`)
 	val.push(`<span style="color: ${col}">${cont}</span><br>`)
 }
