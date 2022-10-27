001f65030d876ffa442febf0e415623e66f2940d
Rhiannon <renii@reniivali.com>
2022-10-27 10:50:18 -0600

    update gang script to handle managing the gang in it's entirety, except for TW. shouldn't be much work left, may do logging soon

--- src/src/gang.js
+++ src/src/gang.js
@@ -22,6 +22,12 @@ export async function main(ns) {
 				if (ns.gang.getGangInformation().isHacking) {
 					if (member.hack < lvlThres) {
 						ns.gang.setMemberTask(members[i], "Train Hacking");
+					} else {
+						if (i == 0) {
+							ns.gang.setMemberTask(members[i], "Ethical Hacking");
+						} else {
+							ns.gang.setMemberTask(members[i], "Money Laundering");
+						}
 					}
 				} else {
 					if (member.agi < lvlThres || member.def < lvlThres || member.dex < lvlThres || member.str < lvlThres) {
@@ -38,29 +44,90 @@ export async function main(ns) {
 			//check multipliers for ascencion
 			for (let i = 0; i < members.length; i++) {
 				const member = ns.gang.getMemberInformation(members[i])
-				//basically, what this says, is that when every stat has passed 4000 exp, based off of 0x asc, we will ascend
-				const expGoalAgi = member.agi_asc_mult * 4000;
-				const expGoalDef = member.def_asc_mult * 4000;
-				const expGoalDex = member.dex_asc_mult * 4000;
-				const expGoalStr = member.str_asc_mult * 4000;
-				if ((member.agi_exp >= expGoalAgi || member.agi >= lvlThres) && member.def_exp >= expGoalDef && member.dex_exp >= expGoalDex && member.str_exp >= expGoalStr) {
-					ns.gang.ascendMember(members[i]);
-					const n = new Notification("Gang", {body: members[i] + " has been ascended!"});
+				//basically, what this says, is that when every stat applicable to the gang type, based off of 0x asc, we will ascend
+				if (ns.gang.getGangInformation().isHacking) {
+					const expGoalHck = member.hack_asc_mult * 4000;
+					if (member.hack_exp >= expGoalHck) {
+						ns.gang.ascendMember(members[i]);
+						const n = new Notification("Gang", { body: `${members[i]} has been ascended!` });
+					}
+				} else {
+					//for COMBAT gangs
+					const expGoalAgi = member.agi_asc_mult * 4000;
+					const expGoalDef = member.def_asc_mult * 4000;
+					const expGoalDex = member.dex_asc_mult * 4000;
+					const expGoalStr = member.str_asc_mult * 4000;
+					if ((member.agi_exp >= expGoalAgi || member.agi >= lvlThres) && member.def_exp >= expGoalDef && member.dex_exp >= expGoalDex && member.str_exp >= expGoalStr) {
+						ns.gang.ascendMember(members[i]);
+						const n = new Notification("Gang", {body: `${members[i]} has been ascended!`});
+					}
 				}
 			}
-			//can we buy some augments?
-			let augs;
+			//can we buy some augments? i think this should work now? hopefully?
+			let augs = [];
 			ns.gang.getEquipmentNames().forEach((name) => {
-				if (ns.gang.getEquipmentType(name) == "Augment") {
-					augs.push({ name: name, stats: ns.gang.getEquipmentStats(name) });
+				if (ns.gang.getEquipmentType(name) == "Augmentation") {
+					augs.push(name);
 				}
 			});
 			const discount = gangHelper.getUpgradeDiscount(ns.gang.getGangInformation().power, ns.gang.getGangInformation().respect);
 			if (discount >= 0.9) {
 				let totalPrice;
-				/*augs.forEach((aug) => {
+				augs.forEach((aug) => {
 					totalPrice += ns.gang.getEquipmentCost(aug);
-				});*/
+				});
+				if (totalPrice * members.length <= ns.getPlayer().money) {
+					augs.forEach((aug) => {
+						members.forEach((m) => {
+							ns.gang.purchaseEquipment(m, aug);
+						});
+					});
+					const n = new Notification("Gang", {body: `Purchased all Augments for every gang member! it cost you $${ns.nFormat(totalPrice, '0,0')}.`});
+				} 
+				//also, check for other equipment depending on gang type
+				if (ns.gang.getGangInformation().isHacking) {
+					let equip = ns.gang.getEquipmentNames();
+					let toBuy = [];
+					let eqp;
+					equip.forEach((e) => {
+						if (ns.gang.getEquipmentType(e) == "Rootkit") {
+							toBuy.push(e);
+						}
+					});
+					//calc total price
+					toBuy.forEach((e) => {
+						eqp += ns.gang.getEquipmentCost(e);
+					});
+					if (eqp * members.length <= ns.getPlayer().money) {
+						toBuy.forEach((e) => {
+							members.forEach((m) => {
+								ns.gang.purchaseEquipment(m, e);
+							});
+						});
+						const n = new Notification("Gang", {body: `Purchased all Rootkits for every gang member! it cost you $${ns.nFormat(eqp, '0,0')}.`});
+					}
+				} else {
+					let equip = ns.gang.getEquipmentNames();
+					let toBuy = [];
+					let eqp;
+					equip.forEach((e) => {
+						if (ns.gang.getEquipmentType(e) == "Armor" || ns.gang.getEquipmentType(e) == "Weapon" || ns.gang.getEquipmentType(e) == "Vehicle") {
+							toBuy.push(e);
+						}
+					});
+					//calc total price
+					toBuy.forEach((e) => {
+						eqp += ns.gang.getEquipmentCost(e);
+					});
+					if (eqp * members.length <= ns.getPlayer().money) {
+						toBuy.forEach((e) => {
+							members.forEach((m) => {
+								ns.gang.purchaseEquipment(m, e);
+							});
+						});
+						const n = new Notification("Gang", {body: `Purchased all Armor, Weapons, and Vehicles for every gang member! it cost you $${ns.nFormat(eqp, '0,0')}.`});
+					}
+				}
 			}
 		} else {
 			ns.tprint("you're not in a gang, dingy");
--- src/test.js
+++ src/test.js
@@ -1,5 +1,14 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /**  @param {import("./src/").NS} ns */
 export async function main(ns) {
-	const test = new Notification('Test', {body: "Hello World!"});
+	//uwu
+	let types = [];
+	ns.gang.getEquipmentNames().forEach((e) => {
+		const eType = ns.gang.getEquipmentType(e);
+		for (let i = 0; i < types.length; i++) {
+			if (types[i] == eType) return;
+		}
+		types.push(eType);
+	});
+	ns.tprint(types);
 }
