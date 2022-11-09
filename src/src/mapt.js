79e4b83b938e186e5258ed3f2965839433d608b0
Rhi <renii@reniivali.com>
2022-11-09 14:17:46 -0700

    update MAPT to use ns exclusively locally

--- src/src/mapt.js
+++ src/src/mapt.js
@@ -4,10 +4,8 @@
   normally, both the name and checkmark will be green, but if you have root but cannot hack (if your level is too low) the name will be yellow.
   if you do not have root access, but can hack, the name will be yellow.
   otherwise, both the name and checkmark will be red.
-  the required hacking level is shown in brackets, with the diffrence to your hacking level shown next to that.
+  the required hacking level is shown in brackets, with the difference to your hacking level shown next to that.
 */
-
-var _ns;
 const col = {
   "r": "\x1b[31m",
   "g": "\x1b[32m",
@@ -24,86 +22,84 @@ export async function main(ns) {
 	while (true) {
 		ns.disableLog("ALL");
 		ns.clearLog();
-		var seenList = [];
-		_ns = ns;
+		let seenList = [];
 		ns.print("╭┐Map ┌────────────────────────────────────────────────────────────────────────────────────────────────╮");
 		ns.print(`│${col.g}R: Home ${col.d}[1]                                                                                           │`);
-		ScanServer("home", seenList, 0, "");
+		ScanServer(ns, "home", seenList, 0, "");
 		ns.print("╰────────────────────────────────────────────────────────────────────────────────────────────────┘Map └╯");
 		await ns.sleep(1000);
 	}
 }
 
-function ScanServer(serverName, seenList, indent, prefix) {
+function ScanServer(ns, serverName, seenList, indent, prefix) {
 	if (seenList.includes(serverName)) return;
 	seenList.push(serverName);
 	
-	var serverList = _ns.scan(serverName);
+	let serverList = ns.scan(serverName);
 	serverList = serverList.filter(function (item) { return seenList.indexOf(item) === -1; });
-	serverList = serverList.sort(ChildCountCompare);
+	serverList = serverList.sort(ChildCountCompare(ns));
 
-	for (var i = 0; i < serverList.length; i++) {
-		var newServer = serverList[i];
+	for (let i = 0; i < serverList.length; i++) {
+		let newServer = serverList[i];
 		if (seenList.includes(newServer)) continue;
-		if (i != serverList.length - 1) {
-			PrintServerInfo(newServer, prefix + "├─")
-			ScanServer(newServer, seenList, indent + 1, prefix + "│ ");
+		if (i !== serverList.length - 1) {
+			PrintServerInfo(ns, newServer, prefix + "├─")
+			ScanServer(ns, newServer, seenList, indent + 1, prefix + "│ ");
 		}
 		else {
-			PrintServerInfo(newServer, prefix + "╰─")
-			ScanServer(newServer, seenList, indent + 1, prefix + "  ");
+			PrintServerInfo(ns, newServer, prefix + "╰─")
+			ScanServer(ns, newServer, seenList, indent + 1, prefix + "  ");
 		}
 	}
 }
 
-function ChildCountCompare(a, b) {
-	var ax = ChildCount(a);
-	var bx = ChildCount(b);
-	return ChildCount(a) > ChildCount(b) ? 1 : -1;
+function ChildCountCompare(ns, a, b) {
+	let ax = ChildCount(ns, a);
+	let bx = ChildCount(ns, b);
+	return ChildCount(ns, a) > ChildCount(ns, b) ? 1 : -1;
 }
 
-function ChildCount(serverName) {
-	var count = 0;
-	var serverList = _ns.scan(serverName);
-	for (var i = 1; i < serverList.length; i++) {
-		count += ChildCount(serverList[i]) + 1;
+function ChildCount(ns, serverName) {
+	let count = 0;
+	let serverList = ns.scan(serverName);
+	for (let i = 1; i < serverList.length; i++) {
+		count += ChildCount(ns, serverList[i]) + 1;
 	}
 	return count;
 }
 
-/** @param {import(".").NS} ns */
-function PrintServerInfo(serverName, prefix) {
-	var hacked = (_ns.hasRootAccess(serverName)) ? "" : "";
-  	//swap these lines from being commented if you're not using a nerd font / don't want fancy marks on the root indicator
-  	//var hacked = (_ns.hasRootAccess(serverName)) ? "Y" : "N";
-	var serverHackingLevel = _ns.getServerRequiredHackingLevel(serverName);
-	var serverRam = _ns.getServerMaxRam(serverName);
-	var hackSkill = _ns.getPlayer()['skills']['hacking'];
-	var hackDiff;
-	var money = [_ns.nFormat(_ns.getServerMoneyAvailable(serverName), '$0,0'), _ns.nFormat(_ns.getServerMaxMoney(serverName), '$0,0')]
+function PrintServerInfo(ns, serverName, prefix) {
+	let hacked = (ns.hasRootAccess(serverName)) ? "" : "";
+	//swap these lines from being commented if you're not using a nerd font / don't want fancy marks on the root indicator
+	//var hacked = (ns.hasRootAccess(serverName)) ? "Y" : "N";
+	let serverHackingLevel = ns.getServerRequiredHackingLevel(serverName);
+	let serverRam = ns.getServerMaxRam(serverName);
+	let hackSkill = ns.getPlayer()['skills']['hacking'];
+	let hackDiff;
+	let money = [ns.nFormat(ns.getServerMoneyAvailable(serverName), '$0,0'), ns.nFormat(ns.getServerMaxMoney(serverName), '$0,0')]
 	if (serverHackingLevel > hackSkill) hackDiff = `-${serverHackingLevel - hackSkill}`;
-	if (serverHackingLevel == hackSkill) hackDiff = 0;
+	if (serverHackingLevel === hackSkill) hackDiff = 0;
 	if (serverHackingLevel < hackSkill) hackDiff = `+${hackSkill - serverHackingLevel}`;
-	var canhack = false;
-	var dfstring = `│${prefix}R:${hacked} ${serverName} ${serverRam}GB [${serverHackingLevel}] ${hackDiff} ${money[0]}/${money[1]}`
+	let canhack = false;
+	let dfstring = `│${prefix}R:${hacked} ${serverName} ${serverRam}GB [${serverHackingLevel}] ${hackDiff} ${money[0]}/${money[1]}`
 	let dfl = dfstring.length
 	let spa = 104 - dfl;
 	let sp = "";
 	for (let i = 0; i < spa; i++) {
 		sp += " "
 	}
-	if (_ns.getHackingLevel() >= serverHackingLevel) {canhack = true}
-	if (_ns.hasRootAccess(serverName)) {
+	if (ns.getHackingLevel() >= serverHackingLevel) {canhack = true}
+	if (ns.hasRootAccess(serverName)) {
 		if (canhack) {
-      _ns.print(`│${prefix}${col.g}R:${hacked} ${serverName}${col.d} ${serverRam}GB [${serverHackingLevel}] ${hackDiff} ${col.y}${money[0]}/${money[1]}${col.d}${sp}│`)
-    } else {
-      _ns.print(`│${prefix}${col.g}R:${hacked}${col.y} ${serverName}${col.d} ${serverRam}GB [${serverHackingLevel}] ${hackDiff} ${col.y}${money[0]}/${money[1]}${col.d}${sp}│`)
-    }
+			ns.print(`│${prefix}${col.g}R:${hacked} ${serverName}${col.d} ${serverRam}GB [${serverHackingLevel}] ${hackDiff} ${col.y}${money[0]}/${money[1]}${col.d}${sp}│`)
+		} else {
+			ns.print(`│${prefix}${col.g}R:${hacked}${col.y} ${serverName}${col.d} ${serverRam}GB [${serverHackingLevel}] ${hackDiff} ${col.y}${money[0]}/${money[1]}${col.d}${sp}│`)
+		}
 	} else {
 		if (canhack) {
-			_ns.print(`│${prefix}${col.r}R:${hacked}${col.y} ${serverName}${col.d} ${serverRam}GB [${serverHackingLevel}] ${hackDiff} ${col.y}${money[0]}/${money[1]}${col.d}${sp}│`)
+			ns.print(`│${prefix}${col.r}R:${hacked}${col.y} ${serverName}${col.d} ${serverRam}GB [${serverHackingLevel}] ${hackDiff} ${col.y}${money[0]}/${money[1]}${col.d}${sp}│`)
 		} else {
-			_ns.print(`│${prefix}${col.r}R:${hacked} ${serverName}${col.d} ${serverRam}GB [${serverHackingLevel}] ${hackDiff} ${col.y}${money[0]}/${money[1]}${col.d}${sp}│`)
+			ns.print(`│${prefix}${col.r}R:${hacked} ${serverName}${col.d} ${serverRam}GB [${serverHackingLevel}] ${hackDiff} ${col.y}${money[0]}/${money[1]}${col.d}${sp}│`)
 		}
 	}
 }
