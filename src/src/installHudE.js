b41ae8acc0eb1e1a85b2a43f866e13ec79f0f75d
Rhi <59777563+reniivali@users.noreply.github.com>
2022-11-13 17:58:59 -0700

    small fix

--- src/src/installHudE.js
+++ src/src/installHudE.js
@@ -3,5 +3,5 @@ export async function main (ns) {
 	await ns.wget('https://raw.githubusercontent.com/reniivali/bitburner-scripts/main/src/src/hud.e.js', 'hud.e.js');
 	await ns.wget('https://raw.githubusercontent.com/reniivali/bitburner-scripts/main/src/src/helpers.e.js', 'helpers.e.js');
 	await ns.wget('https://raw.githubusercontent.com/reniivali/bitburner-scripts/main/src/src/nsg.e.js', 'nsg.e.js');
-	await ns.wget('https://raw.githubusercontent.com/reniivali/bitburner-scripts/main/src/src/glyph.js', 'glyph.e.js');
+	await ns.wget('https://raw.githubusercontent.com/reniivali/bitburner-scripts/main/src/src/glyph.js', 'glyph.js');
 }
